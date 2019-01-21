ad7b023225b5b0ed1f6f583634cc7c23bc58b0e4
Ojka91 <ojkalp@gmail.com>
2019-01-21 16:29:14 +0100

    minor changes. Start doing game.html

--- src/main/resources/static/web/scripts/games.js
+++ src/main/resources/static/web/scripts/games.js
@@ -1,36 +1,45 @@
-fetch('http://localhost:8080/api/games')
-    .then((res) => res.json())
-    .then((json) => {
-        data = json;
-        console.log(data);
-        listOfGames();
-    })
-    .catch((err) => {
-        console.log(err);
-        text.append(data.message);
-    });
+var myApp = new Vue({
+    el: '#app',
+    data: {
+        data: "",
+        json:{
+           
+        },
+    },
 
+    methods: {
+        getData: function () {
 
-function listOfGames() {
-    var gamesList = document.getElementById("games");
-    for (var x = 0; x < data.length; x++) {
-        var li = document.createElement("li");
-        var gameData = new Date(data[x].created);
-        li.append(gameData.toLocaleString() + " ");
+            fetch('/api/games/')
+                .then((res) => res.json())
+                .then((json) => {
+                    this.data = json;
+                    console.log(this.data);
+                    this.init();
 
-        if (data[x].GamePlayers.length > 0) {
-            for (var y = 0; y < data[x].GamePlayers.length; y++) {
-                var player = data[x].GamePlayers[y].player.email
-                if (data[x].GamePlayers.length < 2){
-                    li.append(player + " waiting for opponent")
-                }else{
-                    li.append(player + " ");
-                }
-              
-            }
-         
-            gamesList.append(li);
+                })
+                .catch((err) => {
+                    console.log(err);
+                    text.append(data.message);
+                    console.log("errordimierda");
+                })
+        },
+
+        init: function () {
+            this.getPlayers();
+            
+        },
+
+        getPlayers: function(){
+                     
+            
+
         }
 
-    }
-}
+
+    },
+    created: function () {
+        this.getData();
+    },
+
+});
